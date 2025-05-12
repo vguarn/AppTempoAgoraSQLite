8ffea3a9c882653cfebaabd16ed30740c73e75b7
231172 <231172@CPD02-M14>
2025-05-12 10:48:36 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1/Models/PrevisaoTempo.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+
+public class PrevisaoTempo
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Cidade { get; set; }
+    public string Data { get; set; }
+    public string Temperatura { get; set; }
+    public string Condicao { get; set; } 
+}
